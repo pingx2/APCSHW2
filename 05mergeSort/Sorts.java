52788f497666d0e59cd9af66278c8749e8a2dafc
pingx2 <pingx2.mm@gmail.com>
2015-03-01 15:57:18 -0500

    not done

---
+++ 05mergeSort/Sorts.java
@@ -0,0 +1,27 @@
+public class Sorts{
+
+    public void merge(int[] a, int[] b, int acount, int bcount){
+	int[] merged = new int[a.length+b.length]; 
+	for(int i = 0; i < merged.length; i++){
+	    if(acount < a.length && bcount < b.length){
+		if(a[acount] >= b[bcount]){
+		    merged[i] = b[acount];
+		    bcount++;
+		}
+		else if(a[acount] < b[bcount]){
+		    merged[i] = a[acount];
+		    acount++;
+		}
+	    }else{
+		if(acount == a.length){
+		    merged[i] = b[bcount];
+		    bcount++;
+		}
+		else if(bcount == b.length){
+		    merged[i] = a[acount];
+		    acount++;
+		}
+	    }
+	}
+    }
+}
