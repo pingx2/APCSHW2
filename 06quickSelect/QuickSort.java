e2eaed188f1c827f50223c0ad9d22e103ab8872c
pingx2 <pingx2.mm@gmail.com>
2015-03-10 02:13:46 -0400

    done

--- 06quickSelect/QuickSort.java
+++ 06quickSelect/QuickSort.java
@@ -1,73 +1,83 @@
 import java.util.*;
 
-public class QuickSort{
+public class Sorts{
 
     private static Random r = new Random();
-  
-    public static int quickSelect(int[] ary, int n){
-	if(n <= 0 || n > ary.length){
-	    throw new IndexOutOfBoundsException();
-	}
-	return partition(ary,0,ary.length-1,n); 
+
+    public static void quickSort(int[] ary){
+	quickSort(ary,0,ary.length-1);
     }
 
-     public static int findpartition(int[] ary){
-	 //pick three random elements and pick the median
-	 
-     }
-
-    //make in place
-    public static int partition(int[] ary, int si, int ei, int n){
-	int[] d = new int[ary.length];
-	for(int i = 0; i < si; i++){
-	    d[i] = ary[i];
+    public static void quickSort(int[] ary, int si, int ei){
+	if(si < ei){
+	    int pi = partition(ary,si,ei);
+	    quickSort(ary,si,pi-1);
+	    quickSort(ary,pi+1,ei);
 	}
-	for(int i = ei; i < ary.length; i++){
-	    d[i] = ary[i];
-	}
-	int pi = si + r.nextInt(ei-si+1);
-	int s = si;
-	int e = ei;
-	for(int i = s; i <= e; i++){
-	    if(ary[i] < ary[pi]){
-		d[si] = ary[i];
-		si++;
-	    }
-	    if(ary[i] > ary[pi]){
-		d[ei] = ary[i];
-		ei--;
-	    }
-	}
-	d[si] = ary[pi];
-
-	//System.out.println(Arrays.toString(d));
-
-	
-	if(si == n - 1){
-	    return d[si];
-	}
-
-	if(si < n - 1){
-	    return partition(d,si+1,e,n);
-	}else{
-	    //(si > n - 1){
-	    return partition(d,s,si-1,n);
-	}
-	
     }
     
-    public static void quickSort(int[] a){
-	int pi = findpartition(a);
-    	int s = a[pi];
-	a[pi] = a[a.length-1];
-	a[a.length-1] = s;
-	if(quickSelect(a,
 
+    public static int partition(int[] ary, int si, int ei){
+	int pi = si + r.nextInt(ei-si+1);
+	int pivot = ary[pi];
 
+	System.out.println(pi);
+	System.out.println(pivot);
 
+	ary[pi] = ary[ei];
+	ary[ei] = pivot;
 
+	int s = si;
+	int e = ei-1;
+	int temp;
 
+	System.out.println(Arrays.toString(ary));
 
+	while(s <= e){
+	    if(ary[s] < pivot){
+		s++;
+	    }else if(ary[s] > pivot){
+		temp = ary[s];
+		ary[s] = ary[e];
+		ary[e] = temp;
+		e--;
+	    }else if(ary[s] == pivot){
+		temp = ary[s];
+		ary[s] = ary[s+1];
+		ary[s+1] = temp;
+		s++;
+	    }
+	}
+
+	System.out.println(Arrays.toString(ary));	
+
+	System.out.println(s);
+	System.out.println(e);
+	//System.out.println(pi);
+       	
+      
+	ary[ei] = ary[s];
+        ary[s] = pivot;   
+
+	//System.out.println(Arrays.toString(ary));
+
+	return s;
+    }
+	
+    
+    
+    public static void main(String[]args){
+	
+	int[] ary = new int[15];
+	for (int i = 0; i < ary.length; i++) {
+	    ary[i] = r.nextInt(100);
+	}
+	System.out.println(Arrays.toString(ary));
+
+	quickSort(ary);
+
+	System.out.println(Arrays.toString(ary));
+	
 	
     }
-}
+}  
