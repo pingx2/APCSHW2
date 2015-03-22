f7d15cf16cbe5f9decf015c6ad097fc634f6c493
pingx2 <pingx2.mm@gmail.com>
2015-03-21 23:01:53 -0400

    add exceptions

--- 09stack/MyStack.java
+++ 09stack/MyStack.java
@@ -4,8 +4,17 @@ public class MyStack<E>{
     
     private LinkedList<E> stack;
 
-    public MyStack(){
+    public String name(){
+	return "zeng.pingping";
+    }
 
+    public MyStack(){
+	stack = new LinkedList<E>();
+    }
+
+   public E push(E item){
+       stack.add(0,item);
+       return item;
     }
 
     public E peek(){
@@ -18,15 +27,34 @@ public class MyStack<E>{
 	return current;
     }
 
-    public void push(E item){
-	stack.add(item);
+    public boolean empty(){
+	return (stack.size() == 0);
     }
 
-    public boolean empty(){
-	if(stack.size() == 0){
-	    return true;
-	}
-	return false;
+    public static void main(String[]args){
+	
+	MyStack<String> a = new MyStack<String>();
+
+	a.push("a");
+	a.push("b");
+	a.push("c");
+	a.push("1");
+	a.push("2");
+
+	System.out.println(a.peek());
+	System.out.println(a.pop());
+	System.out.println(a.pop());
+	System.out.println(a.peek());
+
+	System.out.println(a.pop());
+	System.out.println(a.pop());
+	System.out.println(a.pop());
+
+	System.out.println(a.peek());
+
+
+
+
     }
 
 }
