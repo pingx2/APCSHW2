7dff65d2628f97ea24a226a862f69db2010575ec
pingx2 <pingx2.mm@gmail.com>
2015-03-25 12:22:25 -0400

    fixed typecast thing

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -3,51 +3,53 @@ import java.util.*;
 public class MyDeque<T>{
 
     private int head, tail, size;
-    private T[] que;
+    private Object[] que;
 
     public String name(){
 	return "zeng.pingping";
     }
 
     public MyDeque(){
-	que = new T[10];
+	que = new Object[10];
 	head = 4;
 	tail = 5;
 	size = 0;
     }
 
     public void addFirst(T value){
-	resize();
+	//resize();
 	que[head] = value;
 	head--;
 	size++;
     }
 
     public void addLast(T value){
-	resize();
+	//resize();
 	que[tail] = value;
 	tail++;
 	size++;
     }
 
     public T removeFirst(){
-	T res = que[head + 1];
+	T value = (T)que[head + 1];
 	head++;
 	size--;
+	return value;
     }
 
     public T removeLast(){
-	T res = que[tail - 1];
+	T value = (T)que[tail - 1];
 	tail--;
 	size--;
+	return value;
     }
 
     public T getFirst(){
-
+	return (T)que[head + 1];
     }
 
     public T getLast(){
-
+	return (T)que[tail - 1];
     }
 
     public void resize(){
@@ -64,6 +66,7 @@ public class MyDeque<T>{
 	que.addLast(4);
 	que.addLast(5);
 	que.addLast(6);
+	System.out.println(que.getFirst());
     }
 
 }
