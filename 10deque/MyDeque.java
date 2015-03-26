0047bd135f89e8136af89b04fb765c35d82910a0
pingx2 <pingx2.mm@gmail.com>
2015-03-25 21:41:31 -0400

    added exceptions

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -10,27 +10,36 @@ public class MyDeque<T>{
     }
 
     public MyDeque(){
-	que = new Object[10];
-	head = 4;
-	tail = 5;
+	que = new Object[100];
+	head = 49;
+	tail = 50;
 	size = 0;
     }
 
     public void addFirst(T value){
-	//resize();
+	resize();
+	if(head < 0){
+	    head = que.length - 1;
+	}
 	que[head] = value;
 	head--;
 	size++;
     }
 
     public void addLast(T value){
-	//resize();
+	resize();
+	if(tail >= que.length){
+	    tail = 0;
+	}
 	que[tail] = value;
 	tail++;
 	size++;
     }
 
     public T removeFirst(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
 	T value = (T)que[head + 1];
 	head++;
 	size--;
@@ -38,6 +47,9 @@ public class MyDeque<T>{
     }
 
     public T removeLast(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
 	T value = (T)que[tail - 1];
 	tail--;
 	size--;
@@ -45,15 +57,64 @@ public class MyDeque<T>{
     }
 
     public T getFirst(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
 	return (T)que[head + 1];
     }
 
     public T getLast(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
 	return (T)que[tail - 1];
     }
 
     public void resize(){
+	if(size == que.length){
+	    Object[] copy = new Object[que.length * 2];
+	    int start = copy.length/2 - ((tail - head)/2);
+	    if(head < tail){
+		for(int i = head; i <= tail; i++){
+		    copy[start + i] = que[i];
+		}
+		head = start;
+		tail += start;
+	    }else{
+		int j = 0;
+		for(int i = head; i < que.length; i++){
+		    copy[start + j] = que[i];
+		    j++;
+		}
+		for(int i = 0; i <= tail; i++){
+		    copy[start + j] = que[i];
+		    j++;
+		}
+		head = start;
+		tail = start + que.length;
+	    }
+	    que = copy;
+	}
+    }
 
+    public String toString(){
+	if(size > 0){
+	String result = "[";
+	if(head < tail){
+	    for(int i = head + 1; i < tail; i++){
+		result += que[i] + ", ";
+	    }
+	}else{
+	    for(int i = head + 1; i < que.length-1; i++){
+		result += que[i] + ", ";
+	    }
+	    for(int i = 0; i < tail; i++){
+		result += que[i] + ", "; 
+	    }
+	}
+	return result.substring(0,result.length() - 2) + "]";
+	}
+	return "[]";
     }
 
     public static void main(String[]args){
@@ -67,6 +128,9 @@ public class MyDeque<T>{
 	que.addLast(5);
 	que.addLast(6);
 	System.out.println(que.getFirst());
+	System.out.println(que);
+
+
     }
 
 }
