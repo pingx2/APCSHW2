325ecb5d563cf09b1baf1f34f5bafa555c655019
pingx2 <pingx2.mm@gmail.com>
2015-03-31 12:37:06 -0400

    constructor

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -2,10 +2,18 @@ import java.util.*;
 
 public class Maze{
 
+    private static final String clear =  "\033[2J";
+    private static final String hide =  "\033[?25l";
+    private static final String show =  "\033[?25h";
+
     public String name(){
 	return "zeng.pingping";
     }
     
+
+    //frontier class
+
+
     private class Coordinate{
 
 	private int xcor, ycor;
@@ -21,6 +29,62 @@ public class Maze{
 
     private int[][] num;
     private Coordinate[] moves;
+
+    private static final DFS = 1;
+
+
+    public Maze(String filename){
+	startx = -1;
+	starty = -1;
+	String ans = "";
+	try{
+	    Scanner in = new Scanner(new File(filename));
+	    while(in.hasNext()){
+		String line= in.nextLine();
+		if(maxy==0){
+		    maxx=line.length();
+		}
+		maxy++;
+		ans+=line;
+	    }
+	}
+	catch(Exception e){
+	    System.out.println("File: "+filename+" could not be opened.");
+	    e.printStackTrace();
+	    System.exit(0);
+	}
+	maze = new char[maxx][maxy];
+	for(int i=0;i<ans.length();i++){
+	    char c = ans.charAt(i);
+	    maze[i%maxx][i/maxx]= c;
+	    if(c=='S'){
+		startx = i%maxx;
+		starty = i/maxx;
+	    }
+	}
+    }
+
+    public String toString(){
+
+    }
+
+    public boolean solveBFS(boolean animate){
+	
+    }
+
+    public boolean solveDFS(boolean animate){
+	
+    }
+
+    public boolean solveBFS(){
+	return solveBFS(false);
+    }
+
+    public boolean solveDFS(){
+	return solveDFS(false);
+    }
+	
+
 
 
 
