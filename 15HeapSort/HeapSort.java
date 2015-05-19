99d7fad56ac47803f139412359cde9b548496060
pingx2 <pingx2.mm@gmail.com>
2015-05-18 20:59:22 -0400

    sorta sorts?

--- 15HeapSort/HeapSort.java
+++ 15HeapSort/HeapSort.java
@@ -2,41 +2,56 @@ import java.util.*;
 
 public class HeapSort{
 
-    private static void heapify(int[] array){
-	for(int i = array.length; i >= 0; i--){
-	    removeSwap(array, i);
-	}
-    }
-
  
     public static void sort(int[] heap){
 	heapify(heap);
-	removeSwap(heap,0);
-    }
-    
-    private static void removeSwap(int[]heap, int index){
-	if(index*2 - 1 < heap.length && (heap[index] > heap[index*2] ||
-					 heap[index] > heap[index*2+1])){
-	    //int temp = heap[index];
-	    if(heap[index*2] > heap[index*2+1]){
-		swap(heap, index, index*2+1);
-		//heap[index] = heap[index*2+1];
-		//heap[index*2+1] = temp;
-		removeSwap(heap, index*2+1);
-	    }else{
-		swap(heap, index, index*2);
-		//heap[index] = heap[index*2];
-		//heap[index*2] = temp;
-		removeSwap(heap, index*2);
-	    }
+	for(int i = heap.length - 1; i >=0; i--){
+	    swap(heap,0,i);
+	    heapSwap(heap,0,i);
 	}
+	
+    }
+
+   private static void heapify(int[] array){
+	for(int i = array.length/2; i >= 0; i--){
+	    heapSwap(array, i, array.length);
+	}
+	System.out.println(Arrays.toString(array));
     }
     
-   private static void swap(int[] heap, int a, int b){
+   private static void heapSwap(int[]heap, int index, int end){
+	int min = -1;
+	if(index * 2 + 1 < end && (heap[index*2+1] < heap[index] ||
+					   heap[index*2+1] < heap[index])){
+	    if(heap[index*2] < heap[index*2+1]){
+		min = index * 2;
+	    }else{
+		min = index * 2 + 1;
+	    }
+	}else{
+	    min = index;
+	}
+	if(min != index){
+	    swap(heap, index, min);
+	    heapSwap(heap, min, end);
+	}
+	
+    }
+    
+    
+    private static void swap(int[] heap, int a, int b){
 	int temp = heap[a];
 	heap[a] = heap[b];
 	heap[b] = temp;
     }
     
+
+    public static void main(String[]args){
+	
+	int[] array = {3,6,7,2,9,8,1};
+	sort(array);
+	System.out.println(Arrays.toString(array));
+
+    }
 
 }
