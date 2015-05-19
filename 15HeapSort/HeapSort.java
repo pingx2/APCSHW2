b16c0a5e1441fd3e772fc47a037ee4dbd1e2fe7a
pingx2 <pingx2.mm@gmail.com>
2015-05-18 20:18:31 -0400

    didnt compile

--- 15HeapSort/HeapSort.java
+++ 15HeapSort/HeapSort.java
@@ -1,24 +1,46 @@
+import java.util.*;
+
 public class HeapSort{
 
-    public static void sort(int[] heap){
-	if(heap[0] == 0){
-	    throw new NoSuchElementException();
+    private static void heapify(int[] array){
+	for(int i = array.length; i >= 0; i--){
+	    removeSwap(array, i);
 	}
+    }
+
+ 
+    public static void sort(int[] heap){
+	heapify(heap);
 	int value = heap[1];
 	heap[1] = heap[heap[0]];
-	heap[heap[0]] = value;
 	heap[0] -= 1;
-	swap(1);
-
+	removeSwap(1);
+	return value;
     }
-
-
-    public static boolean compare(int a, int b){
-
+    
+    private static void removeSwap(int[]heap, int index){
+	if(index*2 - 1 < heap.length && (heap[index] > heap[index*2] ||
+					 heap[index] > heap[index*2+1])){
+	    //int temp = heap[index];
+	    if(heap[index*2] > heap[index*2+1]){
+		swap(heap, index, index*2+1);
+		//heap[index] = heap[index*2+1];
+		//heap[index*2+1] = temp;
+		removeSwap(index*2+1);
+	    }else{
+		swap(heap, index, index*2);
+		//heap[index] = heap[index*2];
+		//heap[index*2] = temp;
+		removeSwap(index*2);
+	    }
+	}
     }
-
-    private void swap(int index){
-
+    
+   private static void swap(int[] heap, int a, int b){
+	int temp = heap[a];
+	heap[a] = heap[b];
+	heap[b] = temp;
     }
+    
 
 }
